d4d1b0a333dd251f41086af6f5391d9b3037509f
Gurkirat Singh <gurkirat63@gmail.com>
2023-01-10 17:29:25 +1100

    basic layout for body content and did layout inheritance

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from flask import Flask , render_template, url_for
 
 
 app = Flask(__name__)
+posts = [{'author':'Gurkirat', 'post_date':'1 Jan 2023', 'post_title':'Blog 1', 'post_content':'This is content of post 1'},{'author':'Karan', 'post_date':'10 Jan 2023', 'post_title':'Blog 2', 'post_content':'This is content of post 2'}]
 
 
 
 @app.route("/home")
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    return render_template("home.html", posts=posts)
 
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
