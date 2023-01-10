721a1f36e809dc2f6b48e61c255887f028b6180d
Gurkirat Singh <gurkirat63@gmail.com>
2023-01-10 14:57:43 +1100

    Navigation Bar CSS styling

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template, url_for
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/home")
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8003)
